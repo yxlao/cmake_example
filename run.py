e33ed92d608b7ec29c5915b0e05b77088565cdaf
Yixing Lao <yixing.lao@gmail.com>
2019-02-19 13:28:46 -0800

    add dummy func

--- run.py
+++ run.py
@@ -3,4 +3,10 @@ import os
 curr_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(curr_path + "/build")
 import cmake_example
+
+def dummy_func(value):
+        print("inside python dummy_func:", value)
+        return False
+
+cmake_example.dummy_callback(dummy_func, 123)
 print(cmake_example.add(1, 2))
